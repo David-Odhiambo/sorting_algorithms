7fed6eb386c8d67b734a7641e19dfd315d4fa2cd
David-Odhiambo <prof.odhiambodavid@gmail.com>
2022-08-17 21:38:03 +0300

    sorts

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,33 +14,35 @@ void swap_ints(int *a, int *b)
 	*b = tmp;
 }
 
+
 /**
- * bubble_sort - Sort an array of integers in ascending order.
- * @array: An array of integers to sort.
- * @size: The size of the array.
+ * bubble_sort - sorts bubbly
+ * @array: array to sort
+ * @size: size
  *
- * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	bool bubbly = false;
+	size_t i, newsize = size;
+	int swap;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	while (newsize)
 	{
-		bubbly = true;
-		for (i = 0; i < len - 1; i++)
+		swap = 0;
+		for (i = 0; i < (size - 1); i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap_ints(array + i, array + i + 1);
+				swap_ints(&array[i], &array[i + 1]);
 				print_array(array, size);
-				bubbly = false;
+				swap = 1;
 			}
 		}
-		len--;
+		newsize--;
+		if (swap == 0)
+			break;
 	}
 }
